cfe008e869b97b85e10a9aee14b7632c56e4eb6c
leonardonunes3 <leonardo.nunes793@gmail.com>
2022-08-17 11:06:33 -0300

    estrutura base do projeto

---
+++ src/main/java/br/com/kahnguru/ecommercebot/ECommerceBotApplication.java
@@ -0,0 +1,13 @@
+package br.com.kahnguru.ecommercebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kahnguru/ecommercebot/ECommerceBotApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kahnguru.ecommercebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
