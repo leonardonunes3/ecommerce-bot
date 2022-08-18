0c1fe24e1e5bdb9564f2df815d56740b2a0924f9
leonardonunes3 <leonardo.nunes793@gmail.com>
2022-08-18 20:46:28 -0300

    dto produto para request

---
+++ src/main/java/br/com/kahnguru/ecommercebot/dto/ProdutoRequestDto.java
@@ -0,0 +1,15 @@
+package br.com.kahnguru.ecommercebot.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProdutoRequestDto {
+
+    private String nome;
+    private String descricao;
+    private String marca;
+    private Integer ano;
+
+}
